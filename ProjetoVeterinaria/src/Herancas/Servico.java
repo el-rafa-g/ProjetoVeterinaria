7fa610207ce78d847a5f44ae276db2a5aae425e9
el-rafa-g <galdinorafael63@gmail.com>
2025-12-01 18:01:58 -0300

    Update Servico.java

--- ProjetoVeterinaria/src/Herancas/Servico.java
+++ ProjetoVeterinaria/src/Herancas/Servico.java
@@ -3,7 +3,72 @@ package Herancas;
 import Interfaces.Clinica;
 
 public abstract class Servico implements Clinica {
+    protected String nomeAnimal;
+    protected int idadeAnimal;
+    protected int numeroDosesVacina;
+    protected boolean animalExotico;
+    protected String porteAnimal;
 
+    protected String nomeVeterinario;
+    protected int crm;
+
+    public String getNomeAnimal() {
+        return nomeAnimal;
+    }
+
+    public void setNomeAnimal(String nomeAnimal) {
+        this.nomeAnimal = nomeAnimal;
+    }
+
+    public boolean isAnimalExotico() {
+        return animalExotico;
+    }
+
+    public void setAnimalExotico(boolean animalExotico) {
+        this.animalExotico = animalExotico;
+    }
+
+    public int getIdadeAnimal() {
+        return idadeAnimal;
+    }
+
+    public void setIdadeAnimal(int idadeAnimal) {
+        this.idadeAnimal = idadeAnimal;
+    }
+
+    public int getNumeroDosesVacina() {
+        return numeroDosesVacina;
+    }
+
+    public void setNumeroDosesVacina(int numeroDosesVacina) {
+        this.numeroDosesVacina = numeroDosesVacina;
+    }
+
+    public String getNomeVeterinario() {
+        return nomeVeterinario;
+    }
+
+    public String getPorteAnimal() {
+        return porteAnimal;
+    }
+
+    public void setPorteAnimal(String porteAnimal) {
+        this.porteAnimal = porteAnimal;
+    }
+
+    public void setNomeVeterinario(String nomeVeterinario) {
+        this.nomeVeterinario = nomeVeterinario;
+    }
+
+    public int getCrm() {
+        return crm;
+    }
+
+    public void setCrm(int crm) {
+        this.crm = crm;
+    }
+
+    
     @Override
     public abstract void cadastrarAnimal();
 
@@ -13,3 +78,4 @@ public abstract class Servico implements Clinica {
     @Override
     public abstract  void cadastrarMedico();
 }
+
